ee093c815b9205675854f3311e1d43e27d4c9ecf
SeongJongHo <whdgh9595@gmail.com>
2022-02-18 19:39:20 +0900

    [ADD]get-products

---
+++ controllers/productController.js
@@ -0,0 +1,37 @@
+const db = require('../models');
+
+module.exports = {
+    getProducts : (req, res)=>{
+        let Menu = ['채소','샐러드','과일','간편식']
+        let Category = ['쌈채소', '간편채소', '닭가슴살', '오늘의 샐러드', '야채 샐러드', '제철과일', '국산과일', '수입과일', '선식', '샌드위치', '시리얼' ]
+        
+        if(req.query.category) Category = req.query.category;
+        if(req.query.menu) Menu = req.query.menu;
+        
+        db.Product.findAll({
+            include:[{
+                model: db.Category,
+                attributes:['id','name'],
+                where:{
+                    name : Category
+                },
+                include: {
+                    model: db.Menu,
+                    attributes:['id','name'],
+                    where:{
+                        name: Menu
+                    }
+                },
+                },{
+                model: db.Image,
+                attributes: ['url']
+                }
+            ]
+        }).then(result=>{
+            res.status(200).json({
+                result: result,
+                message: "success"
+            })
+        })
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const ProductCtrl = require('../controllers/productController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* Product */
+router.get('/products', ProductCtrl.getProducts)
 
 module.exports = router;
