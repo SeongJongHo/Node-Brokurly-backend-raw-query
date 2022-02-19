7226fecfa96ece538b15ede05e5fd9e881f43870
SeongJongHo <whdgh9595@gmail.com>
2022-02-19 17:06:05 +0900

    [ADD]cart-get,post,update,delete

---
+++ controllers/cartController.js
@@ -0,0 +1,87 @@
+const db = require('../models');
+
+module.exports= {
+    getCart: (req, res)=>{
+        db.Cart.findAll({
+            where: {user_id: req.user},
+            include:{
+                model: db.Product,
+                include: {
+                    model: db.Image,
+                    attributes: ['url']
+                }
+            }
+        }).then(result=>{
+            if(result){
+                res.status(200).json({
+                    message: 'SUCCESS',
+                    result : result
+                })
+            }
+        })
+    },
+    addCart: async(req, res)=>{
+        if(!req.body)return res.status(400).json({message: "BAD_REQUEST"})
+
+        const [cart, created] = await db.Cart.findOrCreate({
+            where: {
+                user_id: req.user,
+                product_id: req.body.product_id
+            },
+            defaults: {
+                quantity: req.body.quantity
+              }
+        })
+        if(created){
+            return res.status(201).json({
+                message: 'CREATED',
+                result: cart
+            })
+        }
+        else{
+            cart.quantity += req.body.quantity;
+            console.log(cart.quantity)
+            cart.save().then(result=>{
+                return res.status(200).json({
+                    message: 'SUCCESS',
+                    result: result
+                })
+            })
+        }
+    },
+    updateCart: (req, res)=>{
+        if(!req.body)return res.status(400).json({message: "BAD_REQUEST"})
+
+        db.Cart.update({
+            quantity: req.body.quantity
+        },{
+        where: {
+            id: req.body.cart_id,
+            }
+        }).then(result=>{
+            return res.status(200).json({
+                message: 'SUCCESS',
+            })
+        }).catch(err=>{
+            if(err){
+                return res.status(400).json({
+                    message: 'INVALID_CART',
+                })
+            }
+        })
+    },
+    deleteCart: (req, res)=>{
+        if(!req.body)return res.status(400).json({message: "BAD_REQUEST"})
+
+        db.Cart.destroy({
+            where: {
+                id: req.body.cart_id
+            }
+        }).then(result=>{
+            return res.status(200).json({
+                message: 'SUCCESS',
+                result: result
+            })
+        })
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const ProductCtrl = require('../controllers/productController');
+const CartCtrl = require('../controllers/cartController');
 const login_required = require('../core/userCertification').login_required;
 
 /* GET home page. */
@@ -11,5 +12,13 @@ router.get('/', function(req, res, next) {
 /* Product */
 router.get('/products', ProductCtrl.getProducts)
 router.get('/product/:id', ProductCtrl.getProduct)
+
+/* Cart */
+router.get('/cart', login_required, CartCtrl.getCart)
+router.post('/cart', login_required, CartCtrl.addCart)
+router.patch('/cart', login_required, CartCtrl.updateCart)
+router.delete('/cart', login_required, CartCtrl.deleteCart)
+
+
 
 module.exports = router;
