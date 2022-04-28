2216547a96c49c7254fe73701c0ee4b6fd3529f8
SeongJongHo <whdgh9595@gmail.com>
2022-04-28 18:19:27 +0900

    MODIFY: cart-controller 수정

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,49 +1,53 @@
 const {cartService} = require('../services');
 
-module.exports= {
-    getCart: async(req, res, next)=>{
-        try{
-            const result = await cartService.getCart(req.user)
+const getCart = async(req, res, next)=>{
+    try{
+        const result = await cartService.getCart(req.user)
 
-            return res.status(200).json({message: 'success', result: result})
-        }
-        catch(err){
-            return res.status(err.status || 400).json({message: err.message || 'error'})
-        }
-    },
-    addCart: async(req, res)=>{
-        try{
-            if(!req.body.product_id)return res.status(400).json({message: "BAD_REQUEST"})
-
-            const result = await cartService.addCart(req.user, req.body.product_id, req.body.quantity)
-
-            return res.status(200).json({message: 'success', result: result})
-        }
-        catch(err){
-            return res.status(err.status || 400).json({message: err.message || 'error'})
-        }  
-    },
-    updateCart: (req, res)=>{
-        if(!req.body)return res.status(400).json({message: "BAD_REQUEST"})
-        try{
-            const result = await cartService.updateCart(req.user, req.body.cart_id, req.body.quantity)
-
-            return res.status(200).json({message: 'success', result: result})
-        }
-        catch(err){
-            return res.status(err.status || 400).json({message: err.message || 'error'})
-        }
-    },
-    deleteCart: (req, res)=>{
-        try{
-            if(!req.body)return res.status(400).json({message: "BAD_REQUEST"})
-
-            const result = await cartService.addCart(req.user, req.body.cart_id)
-
-            return res.status(200).json({message: 'success', result: result})
-        }
-        catch(err){
-            return res.status(err.status || 400).json({message: err.message || 'error'})
-        }
+        return res.status(200).json({message: 'success', result: result})
     }
+    catch(err){
+        return res.status(err.status || 400).json({message: err.message || 'error'})
+    }
+}
+const addCart = async(req, res)=>{
+    try{
+        if(!req.body.product_id)return res.status(400).json({message: "BAD_REQUEST"})
+
+        const result = await cartService.addCart(req.user, req.body.product_id, req.body.quantity)
+
+        return res.status(200).json({message: 'success', result: result})
+    }
+    catch(err){
+        return res.status(err.status || 400).json({message: err.message || 'error'})
+    }  
+}
+const updateCart = (req, res)=>{
+    if(!req.body)return res.status(400).json({message: "BAD_REQUEST"})
+    try{
+        const result = await cartService.updateCart(req.user, req.body.cart_id, req.body.quantity)
+
+        return res.status(200).json({message: 'success', result: result})
+    }
+    catch(err){
+        return res.status(err.status || 400).json({message: err.message || 'error'})
+    }
+}
+const deleteCart = (req, res)=>{
+    try{
+        if(!req.body)return res.status(400).json({message: "BAD_REQUEST"})
+
+        const result = await cartService.addCart(req.user, req.body.cart_id)
+
+        return res.status(200).json({message: 'success', result: result})
+    }
+    catch(err){
+        return res.status(err.status || 400).json({message: err.message || 'error'})
+    }
+}
+module.exports= {
+    getCart,
+    addCart,
+    updateCart,
+    deleteCart,
 }
