956077342b6692c44e42dc1b25d56aa8a6776d37
SeongJongHo <whdgh9595@gmail.com>
2022-04-28 18:28:38 +0900

    MODIFY: product-serivce 수정

--- services/product.service.js
+++ services/product.service.js
@@ -1,45 +1,46 @@
 const db = require('../models');
 
-module.exports = {
-    getProducts : async(menu, category)=>{
-        const products = await db.Product.findAll({
-            include:[{
-                model: db.Category,
+const getProducts = async(menu, category)=>{
+    const products = await db.Product.findAll({
+        include:[{
+            model: db.Category,
+            attributes:['id','name'],
+            where:{
+                id : category
+            },
+            include: {
+                model: db.Menu,
                 attributes:['id','name'],
                 where:{
-                    id : category
-                },
-                include: {
-                    model: db.Menu,
-                    attributes:['id','name'],
-                    where:{
-                        id: menu
-                    }
-                },
-                },{
-                model: db.Image,
-                attributes: ['url']
+                    id: menu
                 }
-            ]
-        }).catch((err)=>{
-            throw {status: 400, message: err.message}
-        })
-
-        return products
-    },
-
-    getProduct: async(id)=>{
-        const product = await db.Product.findOne({
-            where:{
-                id: id
             },
-            include:[{
-                model: db.Image, attributes: ['url']
-            }]
-        }).catch((err)=>{
-            throw {status: 400, message: err.message}
-        })
+            },{
+            model: db.Image,
+            attributes: ['url']
+            }
+        ]
+    }).catch((err)=>{
+        throw {status: 400, message: err.message}
+    })
 
-        return product
-    }
+    return products
+}
+const getProduct = async(id)=>{
+    const product = await db.Product.findOne({
+        where:{
+            id: id
+        },
+        include:[{
+            model: db.Image, attributes: ['url']
+        }]
+    }).catch((err)=>{
+        throw {status: 400, message: err.message}
+    })
+
+    return product
+}
+module.exports = {
+    getProducts,
+    getProduct
 }
