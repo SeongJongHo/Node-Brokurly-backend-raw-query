3ae4d45695557c3d03a02f28b25b4a7ca37206f5
SeongJongHo <whdgh9595@gmail.com>
2022-04-28 18:17:40 +0900

    MODIFY: auth middleware 수정

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,45 +1,28 @@
-const jwt = require('jsonwebtoken');
-const SECRET_KEY = require('../config/config.js').SECRET_KEY;
-const ALGORITHM = require('../config/config.js').ALGORITHM;
 const db = require('../models')
+const {verifyToken} = require('../utils/token.util')
+
+const login_required = async(req, res, next)=>{
+    if(req.headers.authorization){
+        const userId = await verifyToken(req.headers.authorization)
+
+        await db.User.findOne({where: {id: userId}})
+            .then(result=>{
+                if(result){
+                    req.user = result.id
+                    next()
+                }
+                else {
+                    throw {status:401, message: 'invalid user'}
+                }
+            }).catch(()=>{
+                throw {status:401, message: 'invalid user'}
+            })
+    }
+    else {
+        throw {status:401, message: 'invalid user'}
+    }
+}
 
 module.exports = {
-    login_required : async(req, res, next)=>{
-        try{
-            if(req.headers.authorization){
-                const payload = {}
-                jwt.verify(req.headers.authorization, SECRET_KEY, ALGORITHM,
-                    (err, decoded)=>{
-                        if(!err) {
-                            payload.id = decoded.id
-                        }
-                        else {
-                            throw new jwt.JsonWebTokenError('INVALID_TOKEN')
-                        }                     
-                    }
-                )
-                await db.User.findOne({where: {id: payload.id}})
-                    .then(result=>{
-                        if(result){
-                            req.user = result.id
-                            next()
-                        }
-                        else {
-                            throw new Error('INVALID_USER')
-                        }
-                    }).catch(()=>{
-                        throw new Error('INVALID_USER')
-                    })
-            }
-            else {
-                throw new Error('INVALID_AUTHORIZATION')
-            }
-        }
-        catch(err){
-            if(err.message=='INVALID_AUTHORIZATION'){
-                return res.status(401).json({message: err.message, name: err.name})
-            }
-            return res.status(401).json({message: err.message, name: err.name})
-        }
-    }
+    login_required,
 }
