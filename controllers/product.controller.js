f44e8fd75d2c3cfcbcffc3f47496437904614001
SeongJongHo <whdgh9595@gmail.com>
2022-04-28 18:21:34 +0900

    MODIFY: product-controller 수정

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,29 +1,30 @@
 const {productService} = require('../services')
 
-module.exports = {
-    getProducts : (req, res)=>{
-        try{
-            const menu = req.query.menu? req.query.menu : [1,...4]
-            const category = req.query.category? req.query.category : [1,...11];            
-            const result = productService.getProduct(menu, category)
+const getProducts = (req, res)=>{
+    try{
+        const menu = req.query.menu? req.query.menu : [1,...4]
+        const category = req.query.category? req.query.category : [1,...11];            
+        const result = productService.getProduct(menu, category)
 
-            return res.status(200).json({message: 'success', result: result}) 
-        }
-        catch(err){
-            return res.status(err.status || 400).json({message: err.message || 'error'})
-        }
-    },
-
-    getProduct: async(req, res)=>{
-        try{
-            if(!req.params.id) throw {status: 400, message: 'id is is necessary'}
-
-            const result = await product.getProduct(req.params.id)
-
-            return res.status(200).json({message: 'success', result: result}) 
-        }
-        catch(err){
-            return res.status(err.status || 400).json({message: err.message || 'error'})
-        }
+        return res.status(200).json({message: 'success', result: result}) 
     }
+    catch(err){
+        return res.status(err.status || 400).json({message: err.message || 'error'})
+    }
+}
+const getProduct = async(req, res)=>{
+    try{
+        if(!req.params.id) throw {status: 400, message: 'id is is necessary'}
+
+        const result = await product.getProduct(req.params.id)
+
+        return res.status(200).json({message: 'success', result: result}) 
+    }
+    catch(err){
+        return res.status(err.status || 400).json({message: err.message || 'error'})
+    }
+}
+module.exports = {
+    getProducts,
+    getProduct,
 }
