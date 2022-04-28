a1869bb8eafa4a73e1cc706627d37cb9e6110680
SeongJongHo <whdgh9595@gmail.com>
2022-04-28 18:25:01 +0900

    MODIFY: validator-util  수정

--- utils/validator.util.js
+++ utils/validator.util.js
@@ -1,23 +1,26 @@
-module.exports={
-    validates_email: (email)=>{
-        const exptext = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+const validates_email = (email)=>{
+    const exptext = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 
-        if(exptext.test(email)==false){
-            throw {status:400, message: 'invalid email'}
-        }
-    },
-    validates_password: (password)=>{
-        const exptext = /^[a-zA-Z0-9!@#$%^&*+=_]{8,}$/
-
-        if(exptext.test(password)==false){
-            throw {status:400, message: 'invalid password'}
-        }
-    },
-    validates_username: (username)=>{
-        const exptext = /^[a-zA-Z0-9]{6,16}$/
-
-        if(exptext.test(username)==false){
-            throw {status:400, message: 'invalid username'}
-        }
+    if(exptext.test(email)==false){
+        throw {status:400, message: 'invalid email'}
     }
+}
+const validates_password = (password)=>{
+    const exptext = /^[a-zA-Z0-9!@#$%^&*+=_]{8,}$/
+
+    if(exptext.test(password)==false){
+        throw {status:400, message: 'invalid password'}
+    }
+}
+const validates_username = (username)=>{
+    const exptext = /^[a-zA-Z0-9]{6,16}$/
+
+    if(exptext.test(username)==false){
+        throw {status:400, message: 'invalid username'}
+    }
+}
+module.exports={
+    validates_email,
+    validates_password,
+    validates_username,
 }
