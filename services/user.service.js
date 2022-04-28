9df4c7b9818531867b8deede8e7ec495dfeaf11a
SeongJongHo <whdgh9595@gmail.com>
2022-04-28 18:30:06 +0900

    MODIFY: user-serivce 수정

--- services/user.service.js
+++ services/user.service.js
@@ -3,56 +3,59 @@ const db = require('../models');
 const validate = require('../utils/validator.util');
 const {signToken} = require('../utils/token.util')
 
+const checkEmail = async(email)=>{
+    const user = await db.User.findOne({where: {email: email}})
+    
+    if(user) throw {status:400, message: 'email exist'}
+    
+    return user??true
+}
+const checkUsername = (req, res)=>{
+    const user = await db.User.findOne({where: {username: username}})
+    
+    if(user) throw {status:400, message: 'username exist'}
+    
+    return user??true
+}
+const signUp = async(
+    username,
+    email   ,
+    password,
+    address ,
+    name    ,
+    contact ,)=>{
+        validate.validates_email(email)
+        validate.validates_password(password)
+        validate.validates_username(username)
+        
+        const user = db.User.create({
+            username : username,
+            email    : email,
+            password : await bcrypt.hash(password, 12),
+            address  : address,
+            name     : name,
+            contact  : contact  
+        }).catch(err=>{
+            throw {status: 400, message: err.message}
+        })
 
-module.exports={
-    checkEmail: async(email)=>{
-        const user = await db.User.findOne({where: {email: email}})
+        return user
+}
+const signIn = async(username, password)=>{        
+    const user = await db.User.findOne({where: {username: username}})
+    if (!user) throw {status: 400, message: 'invalid username'}
+    
+    const hashPassword = await bcrypt.compare(password, user.password)
+    if (hashPassword == false) throw {status: 400, message: 'invalid password'}
+    else{
+        const token = await signToken(user.id)
         
-        if(user) throw {status:400, message: 'email exist'}
-        
-        return user??true
-    },
-    checkUsername: (req, res)=>{
-        const user = await db.User.findOne({where: {username: username}})
-        
-        if(user) throw {status:400, message: 'username exist'}
-        
-        return user??true
-    },
-    signUp: async(
-        username,
-        email   ,
-        password,
-        address ,
-        name    ,
-        contact ,)=>{
-            validate.validates_email(email)
-            validate.validates_password(password)
-            validate.validates_username(username)
-            
-            const user = db.User.create({
-                username : username,
-                email    : email,
-                password : await bcrypt.hash(password, 12),
-                address  : address,
-                name     : name,
-                contact  : contact  
-            }).catch(err=>{
-                throw {status: 400, message: err.message}
-            })
-
-            return user
-    },
-    signIn: async(username, password)=>{        
-        const user = await db.User.findOne({where: {username: username}})
-        if (!user) throw {status: 400, message: 'invalid username'}
-        
-        const hashPassword = await bcrypt.compare(password, user.password)
-        if (hashPassword == false) throw {status: 400, message: 'invalid password'}
-        else{
-            const token = await signToken(user.id)
-            
-            return token
-        }
+        return token
     }
+}
+module.exports={
+    checkEmail,
+    checkUsername,
+    signUp,
+    signIn,
 }
