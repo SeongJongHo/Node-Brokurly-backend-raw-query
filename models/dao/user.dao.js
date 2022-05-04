3548407c7029e8db3e94e2aa6fd3d112688276be
SeongJongHo <whdgh9595@gmail.com>
2022-05-04 19:29:59 +0900

    MODIFY: dao추가, user query 추가

--- models/dao/user.dao.js
+++ models/dao/user.dao.js
@@ -1,2 +1,41 @@
+const db = require('../models/sequelize');
+
+const checkEmailUserDao = async(email)=>{
+    return await db.sequelize.query(
+        `
+        SELECT email
+        FROM users
+        WHERE email = ${email}
+        `
+    )
+}
+
+const getUserDao = async(username)=>{
+    return await db.sequelize.query(
+        `
+        SELECT username, password
+        FROM users
+        WHERE username = ${username}
+        `
+    )
+}
+const addUserDao = async(
+    username,
+    email   ,
+    password,
+    address ,
+    name    ,
+    contact ,)=>{
+        return await db.sequelize.query(
+            `
+            INSERT INTO users(username, email, password, address, name, contact)
+            VALUES (${username}, ${email}, ${password}, ${address}, ${name}, ${contact})
+            `
+        )
+}
+
 module.exports={
+    addUserDao,
+    getUserDao,
+    checkEmailUserDao,
 }
