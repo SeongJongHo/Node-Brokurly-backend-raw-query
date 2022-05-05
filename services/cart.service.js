67284d93ca2704c99bf74f33710d2900f3d944f8
SeongJongHo <whdgh9595@gmail.com>
2022-05-06 03:43:16 +0900

    MODIFY: order-service 수정

--- services/cart.service.js
+++ services/cart.service.js
@@ -1,69 +1,28 @@
-const db = require('../models/sequelize');
+const {cartDao} = require('../models/dao');
 
 const getCart = async(user)=>{
-    console.log(123)
-    const cart = await db.Cart.findAll({
-        where: {user_id: user},
-        include:{
-            model: db.Product,
-            include: {
-                model: db.Image,
-                attributes: ['url']
-            }
-        }
-    }).catch((err)=>{
-        throw {status: 400, message: err.message}
-    })
+    
+    const cart = await cartDao.getCartDao(user)
+
     return cart
 }
 const addCart = async(user, product_id, quantity)=>{
-    const [cart, created] = await await db.Cart.findOrCreate({
-        where: {
-            user_id: user,
-            product_id: product_id
-        },
-        defaults: {
-            quantity: quantity
-          }
-    }).catch((err)=>{
-        throw {status: 400, message: err.message}
-    })
+    
+    const cart = await cartDao.addCartDao(user, product_id, quantity)
 
-    if(created){
-        return cart
-    }
-    else{
-        cart.quantity += quantity;
-        await cart.save()
-
-        return cart
-    }
+    return cart
 }
 const updateCart = async(user, cart_id)=>{
-    const cart = await db.Cart.update({
-        quantity: req.body.quantity
-    },{
-    where: {
-        id: req.body.cart_id,
-        user_id: user
-        }
-    }).catch((err)=>{
-        throw {status: 400, message: err.message}
-    })
+
+    const cart = await cartDao.updateCartDao(user, cart_id)
 
     return cart
 }
 const deleteCart = async(user, cart_id)=>{
-    const cart = await db.Cart.destroy({
-        where: {
-            id: req.body.cart_id
-        }
-    }).catch((err)=>{
-        throw {status: 400, message: err.message}
-    })
 
-    if(cart[0]>0) return cart
-    else throw {status: 400, message: 'not deleted cart'}
+    const cart = await cartDao.deleteCartDao(user, cart_id)
+
+    return cart
 }
 
 module.exports= {
--- services/order.service.js
+++ services/order.service.js
@@ -1,73 +1,19 @@
-const db = require('../models/sequelize');
-const uuid = require('uuid').v4;
-const orderStatus= {
-    WAIT_DEPOSIT       : 1,   
-    COMPLETION_DEPOSIT : 2,    
-    READY_RELEASE      : 3,  
-    SHIPMENT_COMPLETE  : 4,
-    DELIVERED          : 5,
-    DELAYED_DELIVERY   : 6,
-    DELIVERY_COMPLETED : 7,
-    ORDER_COMPLETE     : 8,
-    ORDER_CANCELLATION : 9
-}
+const {orderDao} = require('../models/dao');
 
 const getOrder = async(user)=>{
-    const order = await db.Order.findAll({
-        where: {
-            users_id: req.user
-        },
-        include: {
-            model: db.OrderStatus,
-            model: db.OrderItem,
-            include:{
-                model: db.OrderItemStatus,
-                model: db.Product,
-                include:{
-                    model: db.Image,
-                    attributes: ['url']
-                }
-            }
-        }
-    }).catch((err)=>{
-        throw {status: 400, message: err.message}
-    })
+    const order = await orderDao.getOrderDao(user)
 
     return order
 }
 const addOrder = async(user, cart_id, t)=>{
-    const cart= await db.Cart.findAll({where: {id: cart_id}}, { transaction: t })
-    if(cart.length < 1) throw {status:400, message: 'invalid cart'}
+    const order = await orderDao.addOrderDao(user, cart_id, t)
 
-    await db.Cart.destroy({where:{id: cart_id}}, { transaction: t })
-        
-    const order = await db.Order.create({
-        order_number: uuid(),            
-        order_status_id: orderStatus.WAIT_DEPOSIT,           
-        users_id: user
-    }, { transaction: t })
-        .catch((err)=>{ throw {status:400, message: err.message}})
-
-    const orderItem = await cart.map(Item=>({
-            product_id: Item.product_id,
-            quantity: Item.quantity,
-            order_id: order.id,
-            order_items_status_id: orderStatus.WAIT_DEPOSIT,
-            tracking_number: uuid()
-    }))
-    await db.OrderItem.bulkCreate(orderItem, { transaction: t })
-        .catch(()=>{ throw {status:400, message: err.message}})
-    
-    await t.commit()
-
-    return true
+    return order
 }
 const updateOrder = async(order_id)=>{        
-    const order = db.Order.update({order_status_id: orderStatus.ORDER_CANCELLATION},{where: {id: order_id}})
-        .catch(()=>{ throw {status:400, message: err.message}})
+    const order = await orderDao.updateOrderDao(order_id)
 
-    if(result[0]>0) return true
-    else throw {status:400, message: 'not updated'}
+    return order
 }
 module.exports = {
     getOrder,
