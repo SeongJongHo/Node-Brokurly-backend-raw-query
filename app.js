0641dae9ffa6ebf1d8da3bdba5394a262c0a31d8
SeongJongHo <whdgh9595@gmail.com>
2022-05-18 20:35:06 +0900

    MODIFY: db sync 주석제거

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
-// db 연결
-// db.sync({force:false})
-//   .then(()=>{
-//     console.log("db 연결 성공")
-//   })
-//   .catch(()=>{
-//     console.log("db 연결 실패")
-//   })
+db 연결
+db.sync({force:false})
+  .then(()=>{
+    console.log("db 연결 성공")
+  })
+  .catch(()=>{
+    console.log("db 연결 실패")
+  })
 
 
 
